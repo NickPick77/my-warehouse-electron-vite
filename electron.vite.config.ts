049ec9103a86d2b1cfd97f12e94e09960a90f982
Nicolò Piccolo <nicolo.piccolo@tui.com>
2024-01-27 01:20:52 +0100

    chore: improve build config

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -4,26 +4,42 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   main: {
-    plugins: [externalizeDepsPlugin()],
+    plugins: [externalizeDepsPlugin(), splitVendorChunkPlugin()],
     build: {
+      minify: true,
       rollupOptions: {
         external: ['sqlite3']
       }
     }
   },
   preload: {
-    plugins: [externalizeDepsPlugin()]
+    plugins: [externalizeDepsPlugin(), splitVendorChunkPlugin()],
+    build: {
+      minify: true
+    }
   },
   renderer: {
     resolve: {
-      alias: {
-        '@renderer': resolve('src/renderer/src')
-      }
+      alias: [
+        {
+          find: '@renderer',
+          replacement: resolve('src/renderer/src')
+        },
+        { find: '@components', replacement: resolve('src/renderer/src/components') },
+        { find: '@views', replacement: resolve('src/renderer/src/views') }
+      ]
     },
     plugins: [vue(), splitVendorChunkPlugin()],
     build: {
+      minify: true,
+      cssMinify: true,
       rollupOptions: {
-        input: resolve(__dirname, 'src/renderer/index.html')
+        input: resolve(__dirname, 'src/renderer/index.html'),
+        output: {
+          chunkFileNames: 'assets/chunk/[hash].js',
+          entryFileNames: 'assets/entry/index.js',
+          assetFileNames: 'assets/[ext]/[hash].[ext]'
+        }
       }
     }
   }
