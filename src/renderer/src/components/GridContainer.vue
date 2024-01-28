54076fed39b941182d1fea25976f45a2668ffe46
Nicolò Piccolo <nicolo.piccolo@tui.com>
2024-01-28 19:53:39 +0100

    feat: add empty result

--- src/renderer/src/components/GridContainer.vue
+++ src/renderer/src/components/GridContainer.vue
@@ -4,15 +4,29 @@ import GridHeading from '@components/GridHeading.vue'
 import ItemRow from '@components/ItemRow.vue'
 
 import { useItemsStore } from '@renderer/store/items/index'
+import { useFiltersStore } from '@renderer/store/filters/index'
 
 const itemsStore = useItemsStore()
+const filtersStore = useFiltersStore()
 
 const suitableItems = computed(() => {
-  if (itemsStore.getFilteredItems.length) {
+  if (itemsStore.getFilteredItems.length || filtersStore.getSearchQuery.length >= 3) {
     return itemsStore.getFilteredItems
   }
 
   return itemsStore.getItems
+})
+
+const emptyResults = computed(() => {
+  if (itemsStore.getFilteredItems.length === 0 && filtersStore.getSearchQuery.length >= 3) {
+    return 'no-filtered'
+  }
+
+  if (itemsStore.getItems.length === 0) {
+    return 'no-items'
+  }
+
+  return ''
 })
 </script>
 
@@ -21,6 +35,11 @@ const suitableItems = computed(() => {
     <GridHeading class="grid__heading" />
     <div v-for="item in suitableItems" :key="item.id" class="grid__item">
       <ItemRow :item-data="item" />
+    </div>
+    <div v-if="emptyResults === 'no-filtered'">Nessun Risultato!</div>
+    <div v-if="emptyResults === 'no-items'" class="empty-results__no-items">
+      <h2>Nessun Prodotto Aggiunto!</h2>
+      <p>Clicca sul pulsante verde in alto a destra per iniziare ad aggiungere i tuoi prodotti</p>
     </div>
   </div>
 </template>
@@ -58,4 +77,28 @@ const suitableItems = computed(() => {
     }
   }
 }
+
+.empty-results {
+  &__no-items {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin-top: 50px;
+    color: var(--mw-primary);
+    text-shadow: 2px black;
+    font-size: 18px;
+
+    & h2 {
+      text-shadow: 0px 0px 1px #0000004a;
+    }
+
+    & p {
+      color: black;
+      font-size: 14px;
+      margin-top: 0;
+    }
+  }
+}
 </style>
+<!-- #2f3241c0; -->
