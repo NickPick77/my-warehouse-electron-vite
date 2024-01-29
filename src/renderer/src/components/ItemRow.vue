441532eba74cd7721fd275bca9bcaf504275c877
Nicolò Piccolo <nicolo.piccolo@tui.com>
2024-01-29 11:04:59 +0100

    fix: remove sq3lite from asarUpack path

--- src/renderer/src/components/ItemRow.vue
+++ src/renderer/src/components/ItemRow.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { computed, ref } from 'vue'
+import { ref } from 'vue'
 
 import CheckBoxToggle from '@components/CheckBoxToggle.vue'
 import RoundIconButton from '@components/RoundIconButton.vue'
